30d10d7d6e603f78ebd31e0b6f555fc4719d7d03
Prasalek, Filip <filipprasalek@gmail.com>
2019-11-28 01:59:18 +0100

    Working canvas

--- src/components/VisualizationDashboard/Visualization.js
+++ src/components/VisualizationDashboard/Visualization.js
@@ -14,6 +14,11 @@ class Visualization extends React.Component {
         this.mapName = 'cracow';
         this.state = {
             pointsOfInterests: [],
+            pedestriants: [
+                {x: 947, y: 759},
+                {x: 947, y: 600},
+                {x: 947, y: 800}
+            ],
             xMouseCoordinate: '-',
             yMouseCoordinate: '-',
             selectedPoi: ''
@@ -26,18 +31,20 @@ class Visualization extends React.Component {
             .then((resp) => resp.json())
             .then((pois) => this.setState({pointsOfInterests: pois}, imageMapResize));
         this.initCanvas();
+        window.addEventListener('resize', this.initCanvas);
     }
 
     drawRect = (coordinates) => {
         let [left, top, right, bot] = coordinates.split(',');
+        this.ctx.fillStyle = 'black';
         this.ctx.globalAlpha = 0.4;
         this.ctx.strokeRect(left, top, right - left, bot - top);
         this.ctx.fillRect(left, top, right - left, bot - top);
-        this.ctx.restore();
     };
 
     drawPoly = (coordinates) => {
         const parsedCoordinates = coordinates.match(/\d+,\d+/g)
+        this.ctx.fillStyle = 'black';
         this.ctx.globalAlpha = 0.4;
         this.ctx.beginPath();
         parsedCoordinates.forEach(coords => {
@@ -47,7 +54,19 @@ class Visualization extends React.Component {
         this.ctx.closePath();
         this.ctx.stroke();
         this.ctx.fill();
-        this.ctx.restore();
+    };
+
+
+    drawPedestrians = () => {
+        const pedestrianSize = this.img.width / this.img.naturalWidth * 7;
+        this.ctx.fillStyle = 'orange';
+        this.ctx.globalAlpha = 1;
+        this.state.pedestriants.forEach(pedestrian => {
+            const width = (this.img.width / this.img.naturalWidth) * pedestrian.x;
+            const height = (this.img.height / this.img.naturalHeight) * pedestrian.y;
+            this.ctx.strokeRect(width, height, pedestrianSize, pedestrianSize);
+            this.ctx.fillRect(width, height, pedestrianSize, pedestrianSize);
+        });
     };
 
     onAreaHover = (e, poiName) => {
@@ -65,12 +84,14 @@ class Visualization extends React.Component {
         this.setState({selectedPoi: '-'});
         // TODO: Widen down rectangle
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.drawPedestrians();
     };
 
     initCanvas = () => {
         this.canvas.width = this.img.clientWidth;
         this.canvas.height = this.img.clientHeight;
         this.ctx = this.canvas.getContext('2d');
+        this.drawPedestrians()
     };
 
     renderAreas = () => {
